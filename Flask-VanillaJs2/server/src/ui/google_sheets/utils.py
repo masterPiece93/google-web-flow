77eb2842262573c05251c07713ce2bb1951b85fd
masterPiece93 <ankit8290@gmail.com>
2025-10-22 12:46:21 +0530

    added the util

--- Flask-VanillaJs2/server/src/ui/google_sheets/utils.py
+++ Flask-VanillaJs2/server/src/ui/google_sheets/utils.py
@@ -1,3 +1,4 @@
+import re
 
 def get_specific_sheets(sheet: object, sheet_id: str, range: str) -> list:
 
@@ -36,3 +37,21 @@ def get_all_sheets_data(sheet: object, sheet_id: str, without_headers: bool =Fal
             _range = _range + "!" +"A2:z999999"
         values.append(get_specific_sheets(sheet, sheet_id, range=_range))
     return values
+
+def extract_id_gid( url) -> tuple:
+        """
+        Extracts the ID and gid from a Google Sheet URL.
+
+        Args:
+            url: The Google Sheet URL.
+
+        Returns:
+            A tuple containing the ID and gid, or (None, None) if not found.
+        """
+        match = re.search(r"/d/([a-zA-Z0-9-_]+)(?:.*?gid=([0-9]+))?", url)
+        if match:
+            sheet_id = match.group(1)
+            gid = match.group(2)
+            return sheet_id, gid
+        return None, None
+    
